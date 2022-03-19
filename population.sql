7eef3f8cc7a5e0316672a33400442319a972fb4e
mannievilla <mannievilla@gmail.com>
2022-03-19 12:31:00 -0500

    changes the csv year columns

---
+++ population.sql
@@ -0,0 +1,15 @@
+Create Table population(
+	Geographic_Area Varchar(100),
+	Census Varchar(100),
+	Estimates_Base Int,
+	year_2010 INT,
+	year_2011 Int,
+	year_2012 Int,
+	year_2013 Int,
+	year_2014 Int,
+	year_2015 Int,
+	year_2016 Int,
+	year_2017 Int,
+	year_2018 Int,
+	year_2019 Int
+	);
